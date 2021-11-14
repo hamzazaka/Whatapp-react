4078e9d938a17ff6fb004e402d822ed3c1774a3c
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-14 18:51:14 -0500

    working on the css of the search bar

--- whatapp-app/src/components/Sidebar.js
+++ whatapp-app/src/components/Sidebar.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from 'react';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, Search, SearchOutlined } from '@material-ui/icons';
 
 export default function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar_header">
-
+            <Avatar/>
+            <div className="sidebar_headerRight">
+                <IconButton>
+                <DonutLarge/>
+                </IconButton>
+                <IconButton>
+                <Chat/>
+                </IconButton>
+                <IconButton>
+                <MoreVert/>
+                </IconButton>
             </div>
-            <div className="sidebar_search"></div>
+            </div>
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                <SearchOutlined/>
+                <input type="text" placeholder='Search or Start a new Chat' />
+                </div>
+            </div>
 
             <div className="sidebar_chats">
-                
+
             </div>
         </div>
     )
