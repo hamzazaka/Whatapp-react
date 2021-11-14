989ea14203bb0f2396cbbba873ad8773b6ae10e2
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-14 18:22:19 -0500

    installing material ui

---
+++ whatapp-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className="sidebar_header">
+
+            </div>
+            <div className="sidebar_search"></div>
+
+            <div className="sidebar_chats">
+                
+            </div>
+        </div>
+    )
+}
